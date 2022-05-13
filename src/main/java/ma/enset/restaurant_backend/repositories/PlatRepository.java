b0db517c656be1eed12b8211bdd51f5cb8b3a49b
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-05-13 16:46:13 +0100

    committ

--- src/main/java/ma/enset/restaurant_backend/repositories/PlatRepository.java
+++ src/main/java/ma/enset/restaurant_backend/repositories/PlatRepository.java
@@ -3,5 +3,8 @@ package ma.enset.restaurant_backend.repositories;
 import ma.enset.restaurant_backend.entities.Plat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlatRepository extends JpaRepository<Plat,Long> {
+    List<Plat> findPlatByCategorie(String categorie);
 }
--- src/main/java/ma/enset/restaurant_backend/web/RestoController.java
+++ src/main/java/ma/enset/restaurant_backend/web/RestoController.java
@@ -1,8 +1,36 @@
 package ma.enset.restaurant_backend.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ma.enset.restaurant_backend.entities.Commande;
+import ma.enset.restaurant_backend.entities.Plat;
+import ma.enset.restaurant_backend.repositories.CategoryRepository;
+import ma.enset.restaurant_backend.repositories.CommandeRepository;
+import ma.enset.restaurant_backend.repositories.PlatRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@Data @AllArgsConstructor
 public class RestoController {
+    private PlatRepository platRepository;
+    private CategoryRepository categoryRepository;
+    private CommandeRepository commandeRepository;
+
+    @GetMapping("/plats")
+    public List<Plat> plats(){
+        return platRepository.findAll();
+    }
+    @GetMapping("/commandes")
+    public  List<Commande> commandes(){
+        return commandeRepository.findAll();
+    }
+    @GetMapping("/platsbycategory/{categorie}")
+    public List<Plat> platsList(@PathVariable String categorie){
+        return platRepository.findPlatByCategorie(categorie);
+    }
 
 }
